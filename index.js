b8d41573a8715c93fa93e4caf476f2ebd7be7d9c
Fyodor Donquixote <mixboksu@gmail.com>
2020-09-20 18:19:35 +0200

    basic styles for header

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ bars.addEventListener('click', () => {
     bars.classList.toggle('hamburger--active');
     document.querySelector('.navigation').classList.toggle("navigation--active")
     document.querySelector('.nav__logo').classList.toggle('nav__logo--active')
-})
+});
