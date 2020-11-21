b4e9e9bd5026ccb6a082696e5c718784c24de5a2
DonquixoteSabo <mixboksu@gmail.com>
2020-11-21 15:10:08 +0100

    fix navbar bug

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ bars.addEventListener('click', () => {
     document.querySelector('.navigation').classList.toggle("navigation--active")
     document.querySelector('.nav__logo').classList.toggle('nav__logo--active')
 });
+
+bars.addEventListener('resize', () => {
+    bars.classList.toggle('hamburger--active');
+    document.querySelector('.navigation').classList.remove("navigation--active")
+    document.querySelector('.nav__logo').classList.remove('nav__logo--active')
+});
